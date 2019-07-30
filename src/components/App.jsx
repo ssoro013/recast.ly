285f5ac3f244d577e9af7226baa0efc7c7d0e742
ssoro013 <ssoro013@gmail.com>
2019-07-29 22:14:01 -0700

    Day 1 of React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,33 @@
+import Search from './Search.js'
+import VideoPlayer from './VideoPlayer.js'
+import VideoList from './VideoList.js'
+import exampleVideoData from '../data/exampleVideoData.js'
+
 var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <Search />
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <VideoPlayer video={exampleVideoData[0]}/>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <VideoList videos={exampleVideoData}/>
       </div>
     </div>
   </div>
 );
 
+// class App extends React.Component{
+//   render()
+// }
+
+
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+
+
